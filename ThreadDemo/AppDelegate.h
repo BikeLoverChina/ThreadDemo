ef592595068d2744ba3c38c61af923f10efac008
YangWenjun <18244688848@163.com>
2018-07-07 22:46:59 +0800

    init

---
+++ ThreadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadDemo
+//
+//  Created by YangWenjun on 2018/7/7.
+//  Copyright © 2018年 YangWenjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadDemo
+//
+//  Created by YangWenjun on 2018/7/7.
+//  Copyright © 2018年 YangWenjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
